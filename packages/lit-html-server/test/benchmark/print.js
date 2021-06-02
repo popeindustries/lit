f8e4c70f9308e68d89a06a9723bee05c1ed83162
Alexander Pope <alex@pope-industries.com>
2021-06-02 11:53:01 +0200

    begin benchmark setup

---
+++ packages/lit-html-server/test/benchmark/print.js
@@ -0,0 +1,18 @@
+import { html, renderToString } from '../../index.js';
+import everything from './the-everything-bagel-template.js';
+import { html as litHtml } from 'lit-html';
+import { render } from '@lit-labs/ssr';
+
+const data = {
+  title: 'title',
+  isTrue: true,
+  number: 42,
+};
+
+if (process.argv[2] === 'ssr') {
+  renderToString(render(everything(litHtml, data))).then((str) =>
+    console.log(str.replaceAll('&lt;', '<').replaceAll('&gt;', '>')),
+  );
+} else {
+  renderToString(everything(html, data)).then(console.log);
+}
---
+++ packages/lit-html-server/test/benchmark/server.js
@@ -0,0 +1,33 @@
+import { html, renderToStream } from '../../index.js';
+import everything from './the-everything-bagel-template.js';
+import http from 'http';
+
+http
+  .createServer((req, res) => {
+    const data = {
+      title: new Date().toISOString(),
+      isTrue: Math.random() > 0.5,
+      number: Math.random() * 100,
+    };
+    res.writeHead(200);
+    const stream = renderToStream(template(data));
+    stream.pipe(res);
+  })
+  .listen(3000);
+
+/**
+ * @param { { title: string, isTrue: boolean, number: number } } data
+ */
+function template(data) {
+  return html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>${data.title}</title>
+      </head>
+      ${everything(html, data)}
+    </html>
+  `;
+}
