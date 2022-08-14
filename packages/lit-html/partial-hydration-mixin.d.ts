4e1b603930838ebc447f050e58b8d2b39150f82f
Alexander Pope <alex@pope-industries.com>
2022-08-14 15:28:36 +0200

    add support for `hydrate:idle` & `hydrate:visible`

--- packages/lit-html/partial-hydration-mixin.d.ts
+++ packages/lit-html/partial-hydration-mixin.d.ts
@@ -1,14 +1,35 @@
 /**
  * Custom element mixin for adding partial/deferred hydration support.
+ *
  */
 export function PartialHydrationMixin<BaseClass extends CustomElementBase>(
   Base: BaseClass,
 ): BaseClass & {
-  prototype: PartialHydration;
-  new (...args: Array<any>): PartialHydration;
+  prototype: PartialHydrationElement;
+  new (...args: Array<any>): PartialHydrationElement;
 };
 
-export class PartialHydration extends CustomElement {}
+export class PartialHydrationElement extends CustomElement {
+  /**
+   * Called when `hydrate:defer` attribute is removed.
+   *
+   * If the element has `hydrate:idle` attribute set,
+   * schedules a call to `triggerConnectedCallback()` when idle
+   * (using `requestIdleCallback`, if available).
+   *
+   * If the element has `hydrate:visible` attribute set,
+   * registers a call to `triggerConnectedCallback()` when visible in the viewport
+   * (using `IntersectionObserver`, if available).
+   */
+  protected handleHydrationReady(): void;
+  /**
+   * Executes `connectedCallback()` on the `BaseClass`.
+   *
+   * It is expected that the `BaseClass` will then eventually trigger hydration
+   * of the element's server rendered content by calling `render(value, this)`.
+   */
+  protected triggerSuperConnectedCallback(): void;
+}
 
 export interface CustomElementBase {
   readonly observedAttributes: Array<string>;
--- packages/lit-html/partial-hydration-mixin.js
+++ packages/lit-html/partial-hydration-mixin.js
@@ -2,12 +2,15 @@
  * @typedef { import('./partial-hydration-mixin.d.js').CustomElementBase } CustomElementBase
  */
 
+/** @type { IntersectionObserver } */
+let intersectionObserver;
+
 /**
  * Custom element mixin for adding partial/deferred hydration support.
  * @param { CustomElementBase } Base
  */
 export function PartialHydrationMixin(Base) {
-  return class extends Base {
+  return class PartialHydrationElement extends Base {
     static get observedAttributes() {
       return [...(super.observedAttributes ?? []), 'hydrate:defer'];
     }
@@ -17,11 +20,13 @@ export function PartialHydrationMixin(Base) {
      */
     constructor() {
       super();
+      /** @type { number | undefined } */
+      this._idleCallbackID = undefined;
     }
 
     connectedCallback() {
       if (!this.hasAttribute('hydrate:defer')) {
-        super.connectedCallback?.();
+        this.handleHydrationReady();
       }
     }
 
@@ -32,9 +37,49 @@ export function PartialHydrationMixin(Base) {
      */
     attributeChangedCallback(name, oldValue, value) {
       if (name === 'hydrate:defer' && value === null) {
-        super.connectedCallback?.();
+        this.handleHydrationReady();
       }
       super.attributeChangedCallback?.(name, oldValue, value);
+    }
+
+    disconnectedCallback() {
+      if (this._idleCallbackID !== undefined) {
+        globalThis.cancelIdleCallback(this._idleCallbackID);
+        this._idleCallbackID = undefined;
+      }
+      if (intersectionObserver !== undefined) {
+        // No exception thrown if not currently observed
+        intersectionObserver.unobserve(this);
+      }
+      super.disconnectedCallback?.();
+    }
+
+    handleHydrationReady() {
+      if (this.hasAttribute('hydrate:idle') && 'requestIdleCallback' in globalThis) {
+        this.removeAttribute('hydrate:idle');
+        this._idleCallbackID = globalThis.requestIdleCallback(() => {
+          this.triggerSuperConnectedCallback();
+        });
+      } else if (this.hasAttribute('hydrate:visible') && 'IntersectionObserver' in globalThis) {
+        if (intersectionObserver === undefined) {
+          intersectionObserver = new IntersectionObserver((entries) => {
+            for (const entry of entries) {
+              if (entry.isIntersecting) {
+                intersectionObserver.unobserve(entry.target);
+                /** @type { PartialHydrationElement } */ (entry.target).triggerSuperConnectedCallback();
+              }
+            }
+          });
+        }
+        this.removeAttribute('hydrate:visible');
+        intersectionObserver.observe(this);
+      } else {
+        this.triggerSuperConnectedCallback();
+      }
+    }
+
+    triggerSuperConnectedCallback() {
+      super.connectedCallback?.();
     }
   };
 }
