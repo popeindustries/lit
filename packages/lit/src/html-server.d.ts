d2b2144ceaa21ad5718d9d58b9e53583bf1f232a
Alexander Pope <alex@pope-industries.com>
2022-09-20 10:38:21 +0200

    fix lit-html-server type export

--- packages/lit/src/html-server.d.ts
+++ packages/lit/src/html-server.d.ts
@@ -1,1 +1,3 @@
-export * from '@popeindustries/lit-html-server';
+// `@popeindustries/lit-html-server` is preferred,
+// but TS isn't happy resolving the default export with `node` condition?
+export * from '@popeindustries/lit-html-server/lit-html-server.js';
