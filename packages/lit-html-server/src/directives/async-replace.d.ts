d58539022b9bda2d5759395b26703db5a220ab6e
Alexander Pope <alex@pope-industries.com>
2022-09-22 10:08:41 +0200

    update JSDocs for server directives

--- packages/lit-html-server/src/directives/async-replace.d.ts
+++ packages/lit-html-server/src/directives/async-replace.d.ts
@@ -2,12 +2,12 @@ import { AsyncDirective } from 'lit-html/async-directive.js';
 import { DirectiveResult } from 'lit-html/directive.js';
 
 declare class AsyncReplaceDirective extends AsyncDirective {
-  /**
-   * Server render "value" asynchronously, rendering the first iterated value before completing
-   */
   render<T>(value: AsyncIterable<T>, mapper?: (value: T, index?: number) => unknown): Promise<unknown>;
 }
 
+/**
+ * Server render "value" asynchronously, rendering the first iterated value before completing
+ */
 export function asyncReplace(
   value: AsyncIterable<unknown>,
   mapper?: (value: unknown, index?: number) => unknown,
